b4fb0ebc696ec68fc3c4568c9120d3cfc97097a9
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-08-06 20:46:55 -0500

    updated js file with all required functionality minus randomnumber reset is not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 
 // variables
-var crystal = {
+const RandomNumbers = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+
+
+let crystal = {
     blue:
     {
       name: "Blue",
@@ -21,15 +24,16 @@ var crystal = {
       name: "Yellow",
       value: 0
     }
+   
   };
 
-const RandomNumbers = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
 let randomnumberdiv = document.getElementById("RandomNumber")
 let winsdiv = document.getElementById("wins")
 let lossesdiv = document.getElementById("losses")
 let scoreDiv = document.getElementById("score")
 let sumOfCrystals = 0;
-
+let losses = 0;
+let wins = 0;
 
 let RandomChoice = RandomNumbers[Math.floor(Math.random()*RandomNumbers.length)]; 
 console.log(RandomChoice);
@@ -39,10 +43,12 @@ function getRandom(min,max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 function startGame(){
     sumOfCrystals = 0;
     crystal.blue.value = getRandom(1,12)
+    crystal.green.value = getRandom(1,12)
+    crystal.red.value = getRandom(1,12)
+    crystal.yellow.value = getRandom(1,12)
     randomnumberdiv.textContent = "Random Number: " + RandomChoice;
     scoreDiv.textContent = "Score: " + sumOfCrystals;
 }
@@ -50,43 +56,54 @@ function startGame(){
 function checkScore(){
     console.log(sumOfCrystals)
     if (sumOfCrystals > RandomChoice) {
-    // this is a loss, ++loss
+        losses++;
+        lossesdiv.textContent = "Losses: " + losses;
+        alert("You lose!")
+        startGame()
     } else if (sumOfCrystals === RandomChoice){
-    //increment wins, call start game
+        wins++;
+        winsdiv.textContent = "Wins: " + wins; 
+        alert("You Win!")
+        startGame()
+
     }
 }
-
-
-
-
-
-
-
-
 
 function addValues(clickCrystal){
     console.log(clickCrystal, sumOfCrystals)
     sumOfCrystals += clickCrystal.value;
-    scoreDiv.textContent = "Score: " + sumOfCrystals;
+    scoreDiv.textContent = "Your Score: " + sumOfCrystals;
     checkScore();
 }
+
 
 
 $("#crystalimage1").on("click", function(){
     console.log("working") 
 
     addValues(crystal.blue)
+    
+})
+
+$("#crystalimage2").on("click", function(){
+    console.log("working") 
+
+    addValues(crystal.green)
+    
+})
+
+$("#crystalimage3").on("click", function(){
+    console.log("working") 
+
+    addValues(crystal.red)
+    
+})
+
+$("#crystalimage4").on("click", function(){
+    console.log("working") 
+
+    addValues(crystal.yellow)
+    
 })
 
 startGame()
-
-
-
-// create an onclick event that addes the value of the crystal clicked to the score
-
-
-
-// create a function that compares score with RandomNumber
-
-
-// create a reset 
